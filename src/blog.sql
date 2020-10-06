de14e34a62c41cb9e4c029cc68466b769fb90891
ACAM023 <sonalpha023@gmail.com>
2020-10-06 17:23:02 +0300

    Working on Database Squery

--- src/blog.sql
+++ src/blog.sql
@@ -7,18 +7,23 @@ CREATE TABLE users (
     ID SERIAL PRIMARY KEY,
     email VARCHAR,
     username VARCHAR,
-    bio INTEGER,
+    bio VARCHAR,
     image BYTEA
 );
 
 -- /* Part to Work on. Very Important */
--- CREATE TABLE articles (
---     ID SERIAL PRIMARY KEY,
---     title VARCHAR,
---     description VARCHAR,
---     body VARCHAR,
---     tagList,
--- );
+CREATE TABLE articles (
+    ID SERIAL PRIMARY KEY,
+    title VARCHAR,
+    description VARCHAR,
+    body VARCHAR,
+    tagList TEXT [],
+    createdAt TIMESTAMP,
+    updatedAt TIMESTAMP,
+    favorited BOOLEAN,
+    favoritesCount NUMERIC,
+    author JSON
+);
 
 INSERT INTO users (email, username, bio, image)
 VALUES ('jake@jake.jake', 'jake', 'I work at statefarm', null);
